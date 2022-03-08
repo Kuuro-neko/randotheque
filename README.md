356dc7252aea1976b64d8ca990ea508d7b6eaa7b
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@etu.iut-tlse3.fr>
2022-03-08 16:29:32 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# randotheque
+### Randothèque
 
+Projet tutoré réalisé durant le DUT Informatique année spéciale
+Développé par Clicheroux Shayne & Gonzalez Oropeza Gilles & Cazal Victor & Gouazé Lucie
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Bibliotheque de fichiers GPX">
+	<meta name="keywords" content="Projet tutoré, GPX, sport, bibliothèque">
+	<meta name="author" content="Clicheroux Shayne & Gonzalez Oropeza Gilles & Cazal Victor & Gouazé Lucie">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Randothèque</title>
+	<link rel="stylesheet" href="./css/index.css">
+	<link rel="icon" href="./images/logo.png" />
+</head>
+<body>
+    <h1>Randothèque</h1>
+    <!-- TO-DO :) -->
+</body>
