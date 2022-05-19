5e68ce7080863f12ec9f8f035e094448d825e114
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@etu.iut-tlse3.fr>
2022-05-19 11:39:29 +0200

    Refactor la balise <head> en /php/balise_head.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$thisPageTitle = "Randothèque - Connexion"; // Titre de l'onglet
+$thisPage = "index"; // Pour lier à la bonne feuille CSS
+
 require 'php/config.php';
 require 'php/connexiondb.php'; // Crée $linkpdo
 
@@ -31,16 +34,10 @@ if(!empty($_POST['login'] && !empty($_POST['mdp'] && isset($_POST['signin'])))) 
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="description" content="Bibliotheque de fichiers GPX">
-	<meta name="keywords" content="Projet tutoré, GPX, sport, bibliothèque">
-	<meta name="author" content="Clicheroux Shayne & Gonzalez Oropeza Gilles & Cazal Victor & Gouazé Lucie">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Randothèque</title>
-	<link rel="stylesheet" href="./css/index.css">
-	<link rel="icon" href="./images/logo.png" />
-</head>
+<?php
+	include 'php/balise_head.php';
+?>
+
 <body>
     <h1>Randothèque</h1>
     <form action="index.php" class="connexion" method="post">
