3db90438c3c2f8f2df0ca08138bef8b48f407537
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@etu.iut-tlse3.fr>
2022-06-10 11:30:51 +0200

    Début de form pour upload les fichiers

--- public_html/import.php
+++ public_html/import.php
@@ -15,9 +15,40 @@ include 'php/deconnexion_utilisateur.php';
 
 <body>
 	<p id="welcome">Importer une trace</p>
+	<form action="" method="post">
+	<label for="gpx">Importer une trace : </label>
+
+	<input type="file"
+		id="gpx" name="gpx"
+		accept="xml/gpx" required>
+	<input type="submit" value="Importer">
+	</form>
+
+	<?php
+	// Store the gpx file in gpx/ folder
+	if (isset($_FILES['gpx'])) {
+		$file = $_FILES['gpx'];
+		$fileName = $file['name'];
+		$fileTmpName = $file['tmp_name'];
+		$fileSize = $file['size'];
+		$fileError = $file['error'];
+		$fileType = $file['type'];
+
+		$fileExt = explode('.', $fileName);
+		$fileActualExt = strtolower(end($fileExt));
+
+		$allowed = array('gpx');
+		
+	
+	}
+
+
+	?>
 </body>
 
 <?php
+
+
 	include 'php/footer.php';
 ?>
 </html>
