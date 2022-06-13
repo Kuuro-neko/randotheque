d54588e4e550fe2b46be649858fdf1f63f418db2
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@mgail.com>
2022-06-13 15:13:56 +0200

    Rendu le menu responsive

--- public_html/php/head.php
+++ public_html/php/head.php
@@ -8,19 +8,26 @@
 			<h1>Randothèque</h1>
 		</div>
 		<div class="boutons">
-			<div class="navigation">
-				<input class="btMenu" type="button" onclick="location.href='acceuil.php';" value="Acceuil"/>
-				<input class="btMenu" type="button" onclick="location.href='recherche.php';" value="Recherche"/>
-				<input class="btMenu" type="button" onclick="location.href='import.php';" value="Importer"/>
-				<input class="btMenu" type="button" onclick="location.href='profil.php?id_util=<?php echo $_SESSION['id_util']; ?>';" value="Mon profil"/>
-				<input class="btMenu" type="button" onclick="location.href='chat.php';" value="Chat"/>
-				<input class="btMenu" type="button" onclick="location.href='apropos.php';" value="A propos"/>
-			</div>
-			<div class="deconnexion">
-				<form action="" method="get">
-					<input type="submit" name="disconnect" value="Déconnexion" class="btDeco">
-				</form>
-			</div>
+			<nav class="menu">
+				<input type="checkbox" id="menuToggle">
+				<label for="menuToggle" class="menu-icon"><img src="./images/menu_icon.png" alt="Menu de navigation déroulant" height="60"></label>
+				<ul>
+					<li><a href="acceuil.php">Accueil</a>
+					</li>
+					<li><a href="recherche.php">Recherche</a>
+					</li>
+					<li><a href="import.php">Importer</a>
+					</li>
+					<li><a href="profil.php?id_util=<?php echo $_SESSION['id_util']; ?>">Mon profil</a>
+					</li>
+					<li><a href="chat.php">Chat</a>
+					</li>
+					<li><a href="apropos.php">À propos</a>
+					</li>
+					<li><a href="acceuil.php?disconnect=true">Déconnexion</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	</div>
 </div>
