e6c5b1d39b97f5203084c97ea38c2d4bddf82b94
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@mgail.com>
2022-06-13 04:21:05 +0200

    Profil edit OK sauf l image. Affichage correct à faire pour les champs du profil

--- public_html/profil.php
+++ public_html/profil.php
@@ -25,6 +25,89 @@ if($_SESSION['id_util'] != $_GET['id_util']) {
 $thisPageTitle = "Randothèque - Profil de ".$userName; // Titre de l'onglet
 $thisPage = "profil"; // Pour lier à la bonne feuille CSS
 
+// Si le formulaire de modification de profil est rempli
+if(isset($_POST['modifier'])) {
+// Si une image a été uploadée
+	if(isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0) {
+		// Récupérer l'extension de l'image
+		$extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+		// Si l'image est un jpg
+		if($extension == "jpg" || $extension == "jpeg") {
+			// Récupérer l'image
+			$image = $_FILES['avatar']['tmp_name'];
+			// Créer un nom unique pour l'image
+			$nomImage = $_SESSION['id_util'].$extension;
+			// Déplacer l'image dans le dossier images
+			move_uploaded_file($image, "images/avatars/".$nomImage);
+		} else {
+			// Sinon, afficher un message d'erreur
+			echo "<script>alert('Votre image doit être un jpg');</script>";
+		}
+	}
+
+	// Si le champ de modification de nom est rempli
+	if(!empty($_POST['nom'])) {
+		// Si le nom est différent du nom actuel
+		if($_POST['nom'] != $userName) {
+			// Modifier le nom dans la base de données
+			$reqNom = $linkpdo->prepare('UPDATE utilisateur
+			SET Nom_d_utilisateur = :nom
+			WHERE Id_Utilisateur = :id_util');
+			$reqNom->execute(array('nom'=>$_POST['nom'], 'id_util'=>$_SESSION['id_util']));
+			$userName = $_POST['nom'];
+		}
+	}
+	// Si le champ de modification de mail est rempli
+	if(!empty($_POST['mail'])) {
+		// Si le mail est différent du mail actuel
+		if($_POST['mail'] != $mail) {
+			// Modifier le mail dans la base de données
+			$reqMail = $linkpdo->prepare('UPDATE utilisateur
+			SET Mail = :mail
+			WHERE Id_Utilisateur = :id_util');
+			$reqMail->execute(array('mail'=>$_POST['mail'], 'id_util'=>$_SESSION['id_util']));
+			$mail = $_POST['mail'];
+		}
+	}
+	// Si le champ de modification de poids est rempli
+	if(!empty($_POST['poids'])) {
+		// Si le poids est différent du poids actuel
+		if($_POST['poids'] != $poids) {
+			// Modifier le poids dans la base de données
+			$reqPoids = $linkpdo->prepare('UPDATE utilisateur
+			SET Poids = :poids
+			WHERE Id_Utilisateur = :id_util');
+			$reqPoids->execute(array('poids'=>$_POST['poids'], 'id_util'=>$_SESSION['id_util']));
+			$poids = $_POST['poids'];
+		}
+	}
+	// Si le champ de modification de taille est rempli
+	if(!empty($_POST['taille'])) {
+		// Si la taille est différente de la taille actuelle
+		if($_POST['taille'] != $taille) {
+			// Modifier la taille dans la base de données
+			$reqTaille = $linkpdo->prepare('UPDATE utilisateur
+			SET Taille = :taille
+			WHERE Id_Utilisateur = :id_util');
+			$reqTaille->execute(array('taille'=>$_POST['taille'], 'id_util'=>$_SESSION['id_util']));
+			$taille = $_POST['taille'];
+		}
+	}
+	// Si le champ de modification de date de naissance est rempli
+	if(!empty($_POST['dateN'])) {
+		// Si la date de naissance est différente de la date de naissance actuelle
+		if($_POST['dateN'] != $dateN) {
+			// Modifier la date de naissance dans la base de données
+			$reqDateN = $linkpdo->prepare('UPDATE utilisateur
+			SET DateN = :dateN
+			WHERE Id_Utilisateur = :id_util');
+			$reqDateN->execute(array('dateN'=>$_POST['dateN'], 'id_util'=>$_SESSION['id_util']));
+			$dateN = $_POST['dateN'];
+		}
+	}
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -36,7 +119,7 @@ $thisPage = "profil"; // Pour lier à la bonne feuille CSS
 ?>
 	<fieldset class="main">
 		<legend class="title">Profil de <?php echo $userName; ?></legend>
-		<form method="post" action="profil.php?id_util="<?php echo $_GET['id_util']; ?>>
+		<form method="post" action="profil.php?id_util=<?php echo $_SESSION['id_util']; ?> ">
 			<fieldset id="contenuProfil" <?php echo $disableEdit; ?>>
 				<div class="col">
 				<?php 
@@ -95,7 +178,7 @@ $thisPage = "profil"; // Pour lier à la bonne feuille CSS
 				if($_SESSION['id_util'] == $_GET['id_util']) {
 				?>
 				<div class="col">
-					<input type="submit" value="Modifier le profil" name="submit">
+					<input type="submit" value="Modifier le profil" name="modifier">
 				</div>
 				<?php
 					}
