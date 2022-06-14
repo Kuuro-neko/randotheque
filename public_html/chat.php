c03645f56645409b4618cef771c80a45be72fe7e
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@mgail.com>
2022-06-14 22:22:30 +0200

    IHM

--- public_html/chat.php
+++ public_html/chat.php
@@ -7,10 +7,6 @@ include 'php/deconnexion_utilisateur.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-	<title>Chat - Customer Module</title>
-	<link type="chat/css" rel="stylesheet" href="chat.css" />
-</head>
 
 <?php
 include 'php/balise_head.php';
@@ -19,85 +15,45 @@ include 'php/head.php';
 ?>
 
 <p>Chat avec d'autres utilisateurs</p> <!-- miaou -->
-<div id="wrapper">
-	<div id="menu">
-		<p class="welcome">Bienvenue, <b><?php echo $_SESSION['nom_util']; ?></b></p>
-		<p class="logout"><a id="exit" href="#">Quitter le chat</a></p>
-		<div style="clear:both"></div>
+<div id="main">
+	<div id="chat_rooms">
+		<div id="menu">
+			<p class="welcome">Vos groupes de chat</b></p>
+			<div style="clear:both"></div>
+		</div>
+
+		<div id="roombox">
+			<?php
+
+			?>
+		</div>
+
+		<form name="createroom" action="creer_chat.php" method="get">
+			<input name="createroom" type="submit"  id="createroom" value="Créer un nouveau groupe"/>
+		</form>
 	</div>
 
-	<div id="chatbox">
-		<?php
-		if(file_exists("log.html") && filesize("log.html") > 0){
-			$handle = fopen("log.html", "r");
-			$contents = fread($handle, filesize("log.html"));
-			fclose($handle);
+	<div id="wrapper">
+		<div id="menu">
+			<p class="welcome">Bienvenue, <b><?php echo $_SESSION['nom_util']; ?></b></p>
+			<p class="logout"><a id="exit" href="#">Quitter le groupe de chat</a></p>
+			<div style="clear:both"></div>
+		</div>
 
-			echo $contents;
-		}
+		<div id="chatbox">
+			<?php
+
+			?>
+		</div>
+
+		<form name="message" action="">
+			<input name="usermsg" type="text" id="usermsg" size="63" />
+			<input name="submitmsg" type="submit"  id="submitmsg" value="Envoyer" />
+			<button type="button" onClick="window.location.reload();">Rafraichir</button>
+		</form>
 		
-		?>
 	</div>
-
-	<form name="message" action="">
-		<input name="usermsg" type="text" id="usermsg" size="63" />
-		<input name="submitmsg" type="submit"  id="submitmsg" value="Envoyer" />
-	</form>
 </div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-<script type="text/javascript">
-// jQuery Document
-$(document).ready(function()
-{
-	//If user wants to end session
-	$("#exit").click(function(){
-		var exit = confirm("Voulez vous vraiment quitter le chat?");
-		if(exit==true){window.location = 'index.php?logout=true';}		
-	});
-
-
-	//If user submits the form
-	$("#submitmsg").click(function(){	
-		var clientmsg = $("#usermsg").val();
-		$.post("post.php", {text: clientmsg});				
-		$("#usermsg").attr("value", "");
-		return false;
-	});
-
-	//Load the file containing the chat log
-	function loadLog(){		
-
-		$.ajax({
-			url: "log.html",
-			cache: false,
-			success: function(html){		
-				$("#chatbox").html(html); //Insert chat log into the #chatbox div				
-		  	},
-		});
-	}
-
-	//Load the file containing the chat log
-	function loadLog(){		
-		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-		$.ajax({
-			url: "log.html",
-			cache: false,
-			success: function(html){		
-				$("#chatbox").html(html); //Insert chat log into the #chatbox div	
-				
-				//Auto-scroll			
-				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-				if(newscrollHeight > oldscrollHeight){
-					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-				}				
-		  	},
-		});
-
-	}
-	setInterval (loadLog, 2500);
-
-});
-</script>
 
 </body>
 
