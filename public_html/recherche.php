8b5d6d366f1971c220802bbd8c91d910f7c808e7
Gonzalez Oropeza Gilles <gilles.gonzalezoropeza@gmail.com>
2022-06-15 23:12:07 +0200

    Tri des colonnes de rechercher trace

--- public_html/recherche.php
+++ public_html/recherche.php
@@ -114,8 +114,15 @@ include 'php/deconnexion_utilisateur.php';
 				// Afficher les résultats de la recherche
 				if(isset($resultat)){
 
-					echo "<table class=\"gpx_table\">";
-					echo "<tr><th>Description</th><th>Type de sport</th><th>Difficulté</th><th>Localisation</th><th>Distance</th><th>Propriétaire</th><th>Visualiser la trace</th></tr>";
+					echo "<table id=\"gpx_table\">";
+					echo "<tr>
+					<th onclick=\"sortTable(0)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par ordre alphabétique\">Description</th>
+					<th onclick=\"sortTable(1)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par ordre alphabétique\">Type de sport</th>
+					<th onclick=\"sortTable(2)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par difficulté croissante\">Difficulté</th>
+					<th onclick=\"sortTable(3)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par ordre alphabétique\">Localisation</th>
+					<th onclick=\"sortTable(4)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par distance croissante\">Distance</th>
+					<th onclick=\"sortTable(5)\" style=\"cursor: pointer;\" data-toggle=\"tooltip\" title=\"Trier par ordre alphabétique\">Propriétaire</th>
+					<th>Visualiser la trace</th></tr>";
 					foreach($resultat as $gpx) {
 						// Afficher la Description, le Type_de_sport, la Difficulté et la Localisation du $gpx dans les lignes du tableau
 						echo "<tr><td>".$gpx['Description']."</td><td>".$gpx['Type_de_sport']."</td><td>".$gpx['Difficulte']."</td><td>".$gpx['Localisation']."</td><td>".round($gpx['Distance'], 2)." km</td><td><a href=\"profil.php?id_util=".$gpx['Id_Utilisateur']."\">".$gpx['Nom_d_utilisateur']."</a><td><a href=\"visualisation.php?id_gpx=".$gpx['Id_Fichier_GPX']."\">Visualiser</a></td></tr>";
@@ -130,6 +137,11 @@ include 'php/deconnexion_utilisateur.php';
 	include 'php/footer.php';
 ?>
 </body>
+<script>
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
 <script type="text/javascript">
 	/* Double range slider credits : https://codepen.io/joosts/pen/rNLdxvK */
 	var thumbsize = 14;
@@ -245,5 +257,72 @@ include 'php/deconnexion_utilisateur.php';
 		init(slider);
 	});
 	/* End double range slider JS code */
+</script>
+
+<script type="text/javascript">
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("gpx_table");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+		if (n != 4) {
+			if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+			  //if so, mark as a switch and break the loop:
+			  shouldSwitch= true;
+			  break;
+			}
+		} else {
+			// Enlever le km de la chaine de charactères pour comparer les nombres
+			x_num = parseFloat(x.innerHTML.replace(/[^0-9\.]/g, ''));
+			y_num = parseFloat(y.innerHTML.replace(/[^0-9\.]/g, ''));
+			if (x_num > y_num) {
+				shouldSwitch = true;
+				break;
+			}
+		}
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
 </script>
 </html>
